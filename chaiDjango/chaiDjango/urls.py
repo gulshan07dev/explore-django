06d782360ee3bb019ad08824589ea5947f6257a6
gulshan07dev <gulshanku9135@gmail.com>
2024-06-21 18:32:02 +0530

    add urls, views, jinja templates and static css

--- chaiDjango/chaiDjango/urls.py
+++ chaiDjango/chaiDjango/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from . import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("", views.home),
+    path("about/", views.about),
+    path("contact/", views.contact),
+
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
